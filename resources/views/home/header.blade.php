ca541ca216f0c7b0a0e2514c7c68968e112cbfcf
yigitbaris <bariskaynakoglu01@gmail.com>
2022-04-06 18:38:19 +0300

    home page integration

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -6,3 +6,19 @@
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
         
+        <!--NAV beginning -->
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+	          <li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
+	          <li class="nav-item"><a href="car.html" class="nav-link">Cars</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
