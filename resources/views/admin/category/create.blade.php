91797af8e1db8f923925840005feb71a82f09700
yigitbaris <bariskaynakoglu01@gmail.com>
2022-04-25 00:11:08 +0300

    fix admin-create page

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -4,56 +4,51 @@
  
 @section('content')
 	<!--content-->
-    <main class="content">
+    <div class="dashboard-wrapper">
+            <div class="container-fluid dashboard-content">
+            <h1 class="h3 mb-3"><p>Add Category</p></h1>
 
-<div class="container-fluid p-0">
-
-    <h1 class="h3 mb-3"><p>Add Category</p></h1>
-
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <form role="form" action="/admin/category/store" method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label for="exampleInputName1"><h4 class="card-title">Title</h4></label>
-                            <input type="text" class="form-control" name="title" placeholder="Title">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail3"><h4 class="card-title">Keywords</h4></label>
-                            <input type="text" class="form-control" name="keywords" placeholder="Keywords">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword4"><h4 class="card-title">Description</h4></label>
-                            <input type="text" class="form-control" name="description" placeholder="Description">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleSelectGender"><h4 class="card-title">Status</h4></label>
-                            <select class="form-control" name="status">
-                                <option>True</option>
-                                <option>False</option>
-                            </select>
-                        </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <form role="form" action="/admin/category/store" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleInputName1"><h4 class="card-title">Title</h4></label>
+                        <input type="text" class="form-control" name="title" placeholder="Title">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail3"><h4 class="card-title">Keywords</h4></label>
+                        <input type="text" class="form-control" name="keywords" placeholder="Keywords">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword4"><h4 class="card-title">Description</h4></label>
+                        <input type="text" class="form-control" name="description" placeholder="Description">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleSelectGender"><h4 class="card-title">Status</h4></label>
+                        <select class="form-control" name="status">
+                            <option>True</option>
+                            <option>False</option>
+                        </select>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label style="height:33px  ;border-style:solid;background-color:beige; border: 2px solid bisque; border-radius: 8px; padding: 5px;">Image</label>
+                        <input type="file" name="image" class="file-upload-default" style="text-align: center; height:35px  ;border-style:solid;background-color:gainsboro; border: 2px solid gainsboro; border-radius: 8px; padding: 5px;"   >
                         <br>
-                        <div class="form-group">
-                            <label style="height:33px  ;border-style:solid;background-color:beige; border: 2px solid bisque; border-radius: 8px; padding: 5px;">Image</label>
-                            <input type="file" name="image" class="file-upload-default" style="text-align: center; height:35px  ;border-style:solid;background-color:gainsboro; border: 2px solid gainsboro; border-radius: 8px; padding: 5px;"   >
-                            <br>
-                            <div class="input-group col-xs-12" style="max-width: 1000px;">
-                                <input type="text" class="form-control file-upload-info" disabled="" placeholder="Choose Image">
-                                <span class="input-group-append">
-                    <button type="button" class="btn btn-primary">Upload</button>
-                  </span>
-                  <button type="submit" style="border-style:solid;background-color:red; border: 2px solid red; border-radius: 6px; padding: 5px;height:33px">Save</button>
-                            </div>
+                        <div class="input-group col-xs-12" style="max-width: 1000px;">
+                            <input type="text" class="form-control file-upload-info" disabled="" placeholder="Choose Image">
+                            <span class="input-group-append">
+                <button type="button" class="btn btn-primary">Upload</button>
+              </span>
+              <button type="submit" style="border-style:solid;background-color:red; border: 2px solid red; border-radius: 6px; padding: 5px;height:33px">Save</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-
-</div>
-</main>
+                </div>
+            </div>
 @endsection
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,61 +4,46 @@
 
 @section('content')
 <!--content-->
-<main class="content">
-		<div class="container-fluid p-0">
-			<a href="/admin/category/create"class="btn btn-primary">Add Category</a>
-			
-			<div class="row">
-			<br>
-				<div class="col-12">
-					<div class="card">
-					<div class="card-body">
-						
-			<h4 class="card-title">Category List</h4>
-			<div class="table-responsive">
-				<table class="table">
-				<thead>
-					<tr>
-					<th style="width:10px">Id</th>
-					<th>Title</th>
-					<th>Keywords</th>
-					<th>Description</th>
-					<th>Image</th>
-					<th>Status</th>
-					<th style="width:50px">Edit</th>
-					<th style="width:50px">Delete</th>
-					<th style="width:50px">Show</th>
-					</tr>
-				</thead>
-				
-			<tbody>
-			@foreach( $data as $rs )
-				<tr>
-				<td>{{$rs->id}}</td>
-				<td>{{$rs->title}}</td>
-				<td>{{$rs->keywords}}</td>
-				<td>{{$rs->description}}</td>
-				<td>{{$rs->image}}</td>
-				<td>{{$rs->status}}</td>
-				<td><a href="/admin/category/edit/{{$rs->id}}"class="btn btn-primary">Edit </a></td>
-				<td><a href="/admin/category/delete/{{$rs->id}}"class="btn btn-danger"
-						onclick="return confirm('Deleting !! Are you sure?')">Delete </a></td>
-				<td><a href="/admin/category/show/{{$rs->id}}"class="btn btn-success">Show </a></td>
-				<td><label class="badge badge-danger">Pending</label></td>
-				</tr>
-			
-			@endforeach
-			</tbody>
-			</table>
-		</div>
-			</div>
-						<div class="card-body">
-						</div>
-					</div>
-				</div>
-			</div>
+<div class="dashboard-wrapper">
+	<div class="container-fluid dashboard-content">
+		<div class="row">
+		<div class="card-body">
+		<h4 class="card-title">Category List</h4>
+							<table class="table table-striped" style="background-color: white;">
+								<thead>
+									<tr>
+										<th scope="col">Id</th>
+										<th scope="col">Title</th>
+										<th scope="col">Keywords</th>
+										<th scope="col">Description</th>
+										<th scope="col">Image</th>
+										<th scope="col">Status</th>
+										<th scope="col">Edit</th>
+										<th scope="col">Delete</th>
+										<th scope="col">Show</th>
+									</tr>
+								</thead>
 
+								<tbody>
+	@foreach( $data as $rs )
+		<tr>
+		<td>{{$rs->id}}</td>
+		<td>{{$rs->title}}</td>
+		<td>{{$rs->keywords}}</td>
+		<td>{{$rs->description}}</td>
+		<td>{{$rs->image}}</td>
+		<td>{{$rs->status}}</td>
+		<td><a href="/admin/category/edit/{{$rs->id}}"class="btn btn-primary">Edit </a></td>
+		<td><a href="/admin/category/delete/{{$rs->id}}"class="btn btn-danger"
+				onclick="return confirm('Deleting !! Are you sure?')">Delete </a></td>
+		<td><a href="/admin/category/show/{{$rs->id}}"class="btn btn-success">Show </a></td>
+		</tr>
+	
+	@endforeach
+	</tbody>
+	</table>
+			</div>
 		</div>
-	</main>
+	</div>
 
 @endsection
